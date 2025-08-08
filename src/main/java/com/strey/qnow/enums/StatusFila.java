7b4c909089c101af77e97f0b8f60d687ce90e305
Michelle <micastrey@gmail.com>
2025-08-07 22:18:36 -0300

    feat: implementa enums na fila de atendimento

---
+++ src/main/java/com/strey/qnow/enums/StatusFila.java
@@ -0,0 +1,7 @@
+package com.strey.qnow.enums;
+
+public enum StatusFila {
+
+	ATIVA, 
+	INATIVA
+}
--- src/main/java/com/strey/qnow/model/Fila.java
+++ src/main/java/com/strey/qnow/model/Fila.java
@@ -1,5 +1,64 @@
 package com.strey.qnow.model;
 
+import java.time.LocalDateTime;
+
+import com.strey.qnow.enums.StatusFila;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "filas")
 public class Fila {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idFila;
+	
+	@Column(nullable = false)
+	private String filaPrincipal;
+	
+	@Column(name = "criada_em", nullable =false, updatable = false)
+	private LocalDateTime criadaEm;
+	@Column(name = "alterada_em", nullable = true)
+	private LocalDateTime alteradaEm;
+	@Column(name = "finalizada_em", nullable = true)
+	private LocalDateTime finalizadaEm;
+	
+	@PrePersist
+	public void prePersist() {
+	    criadaEm = LocalDateTime.now();
+	    
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		alteradaEm = LocalDateTime.now();
+	}
+	
+	@ManyToOne
+	private Loja loja;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable=false)
+	private StatusFila status = StatusFila.INATIVA;
 
 }
